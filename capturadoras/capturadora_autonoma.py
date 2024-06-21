f036c424375a974987113defcdf96b9e45f00c43
AntonioPayar <antoniopayar7@gmail.com>
2024-06-21 17:18:50 +0200

    bool_movimiento_detectado en capturadora_autonoma

--- capturadoras/capturadora_autonoma.py
+++ capturadoras/capturadora_autonoma.py
@@ -1,5 +1,12 @@
 from capturadoras.capturadora_utils import *
 
+lista_frames_manual_mapa = [None,None,None]
+lista_frames_manual_pov = [None,None,None]
+lisa_moviminetos = [None,None,None]
+numero_listas = 0
+
+movimineto_previo_detectado = [0,0,0]
+bool_movimiento_detectado = False
 
 def modo_autonomo_moviminetos():
 
@@ -43,6 +50,9 @@ def modo_autonomo_moviminetos():
             comun_file.cola_imagenes_map.put(lista_img_mini)
             comun_file.cola_imagenes_pov.put(lista_img_pov)
             comun_file.cola_mov_raton.put(moviminento)    #Enviamos la actividad del usuario a la interfaz grafica
+
+            comprobar_movimineto(moviminento)
+            guardar_frames_previos(lista_img_mini,lista_img_pov,moviminento)
             
             i = 0
             lista_img_mini = [None, None,None, None,None]
@@ -54,7 +64,55 @@ def modo_autonomo_moviminetos():
         # Salir del bucle si se presiona el boton finalizar
         if comun_file.get_Finalizacion == True:
             break
+    
+def comprobar_movimineto(movimiento):
+    global movimineto_previo_detectado
+    global bool_movimiento_detectado
 
+    if movimiento != movimineto_previo_detectado:
+        movimineto_previo_detectado = movimiento
+        bool_movimiento_detectado = True
+    else:
+        bool_movimiento_detectado = False
+
+
+    """
+    Funcion que se encarga de guardar los ultimos segundos antes de activar el modo manual
+    
+    Almacenamos 3 listas de 5 framas previos
+    Si el usuario activa el modo manual guardamos las imagens y la ruta en csv 
+
+    Si el usuario no activa el modo manual no guardamos nada y borramos las imagenes y las sustituimos 
+    por la siguientes
+
+    Parámetros:
+    lista_img_mini (list): Lista con los 5 frames capturados del minimapa
+    lista_img_pov (list): Lista con los 5 frames capturados del pov
+    moviminento (list): movimiento realizado en esos 5 frames
+    """
+def guardar_frames_previos(lista_img_mini,lista_img_pov,moviminento):
+    global lista_frames_manual_mapa
+    global lista_frames_manual_pov
+    global lisa_moviminetos
+    global numero_listas
+
+    if numero_listas < 3:
+        print(numero_listas)
+        lista_frames_manual_mapa[numero_listas] = lista_img_mini
+        lista_frames_manual_pov[numero_listas] = lista_img_pov
+        lisa_moviminetos[numero_listas] = moviminento
+        numero_listas = numero_listas + 1
+    else:
+        lista_frames_manual_mapa = [None,None,None]
+        lista_frames_manual_pov = [None,None,None]
+        lisa_moviminetos = [None,None,None]
+        numero_listas = 0   
+
+
+    """
+    Funcion que se encarga de monitorizar el raton y el teclado para activar el modo manual
+    
+    """
 def detector_movimiento():
     global exit_event
     global keys_number
